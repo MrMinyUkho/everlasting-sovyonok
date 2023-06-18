d6097a890845797b19c7811a1808442c1fe8d271
MrMinyUkho <ADimaso007@gmail.com>
2023-06-18 20:50:33 +0300

    На верочку

--- EverlastingSovyonok/scripts/map/LevelScript.gd
+++ EverlastingSovyonok/scripts/map/LevelScript.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @onready var level  = get_node("./bus_stop_tilemap")
 @onready var Glob   = get_node("/root/Global")
+@onready var map    = get_node("./Sprite2D")
 
 class SortByY:
 	static func sort_ascending(a, b):
@@ -17,6 +18,6 @@ func _process(delta):
 	children.sort_custom(Callable(SortByY,"sort_ascending"))
 	
 	for i in range(len(children)):
-		if children[i] != level:
-			children[i].z_index = i - 4094
+		if children[i] != level and children[i] != map:
+			children[i].z_index = i - 4093
 	
